5a93720a13c2f2bfac3e9f0c528c745d02187c43
Imani Ward <idward712@gmail.com>
2023-04-21 13:23:53 -0400

    Initial Commit

---
+++ QuestionBot/QuestionAnswerer.swift
@@ -0,0 +1,6 @@
+struct MyQuestionAnswerer {
+    func responseTo(question: String) -> String {
+        // TODO: Write a response
+        return "I'm sorry, I don't understand the question"
+    }
+}
