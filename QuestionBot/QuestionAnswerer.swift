24d6980c565cfbda6cb2f16993549baafe1091a8
Imani Ward <idward712@gmail.com>
2023-04-21 13:39:54 -0400

    oo

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,24 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
-        // TODO: Write a response
-        return "I'm sorry, I don't understand the question"
+        if question == "Hello there" || question == "hello there"{
+            return "Why, hello there!"
+        }
+        else if question == "Where should I go on holiday?" || question == "Where can I find the North Pole?"{
+            return "To the North!"
+        }
+        else if question == "Where are the cookies?"
+        {
+            return "In the cookie jar!"
+        }
+        else{
+            return "I'm sorry, I don't understand the question"
+        }
+        
+        
+        
+        
+        
+        
     }
 }
+
